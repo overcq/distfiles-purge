4c281336a4f90b999e032a8b3e834301ab299db9
Janusz Augustyński <ocq@tutanota.com>
2024-03-02 12:57:11 +0100

    Poprawione wykluczanie ‘patchy’

--- distfiles-purge.pl
+++ distfiles-purge.pl
@@ -7,28 +7,28 @@
 #         program główny
 # ©overcq                on ‟Gentoo Linux 17.1” “x86_64”             2021‒8‒30 h
 #*******************************************************************************
-use warnings;
-use strict;
+use v5.35;
 use sigtrap qw(die INT QUIT TERM);
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 my $Q_files_S_dir_distfiles = '/usr/portage/distfiles';
 my $Q_files_S_dir_tmp_distfiles = '/var/tmp/portage/distfiles';
 my $Q_files_S_re_ignore_after_numeric = 'alpha|rc|source|src';
 my $Q_files_S_re_patches = 'patch(?:es|set)?|bug';
+my $Q_files_S_re_version_1 = 'chromium';
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-my @Q_files_I_grep_last_S_last;
+my @Q_files_I_grep_last_S_last = ();
 my @Q_files_S_preserve;
 #===============================================================================
 sub Q_files_I_prepare_name
 {   local $_ = shift;
-    s`%([0-9a-f]{2})` chr( oct( "0x$1" )) `egis;
+    s`%([0-9a-f]{2})` chr( oct( "0x$1" )) `egi;
     s`\.(?:bz2?|crate|deb|exe|gz|jar|rpm|tar(?:\.(?:bz2?|gz|xz))?|t(?:bz2?|gz|xz)|xz|zip)$``is;
     return $_;
 }
 sub Q_files_I_split
 {   local $_ = shift;
-    s`\+{2}`plusplus`gs;
-    s`\+([-_.])`plus$1`gs;
+    s`\+{2}`plusplus`g;
+    s`\+([-_.])`plus$1`g;
     return split /[-_.+]/, lc;
 }
 #-------------------------------------------------------------------------------
@@ -60,8 +60,8 @@ sub Q_files_I_sort_I_cmp
         return $ret if $ret;
     }
     for( my $i = 0; $i != @a_ and $i != @b_; $i++ )
-    {   if( $a_[ $i ] =~ /^[0-9]+$/
-        and  $b_[ $i ] =~ /^[0-9]+$/
+    {   if( $a_[ $i ] =~ /^[0-9]+$/s
+        and  $b_[ $i ] =~ /^[0-9]+$/s
         )
         {   $ret = $a_[ $i ] <=> $b_[ $i ];
             return $ret if $ret;
@@ -99,53 +99,53 @@ sub Q_files_I_grep_split
 sub Q_files_I_grep_last
 {   my $file_root = Q_files_I_prepare_name(shift);
     local @_ = Q_files_I_grep_split( $file_root );
-    my $ret = 0;
     if( @_ == @Q_files_I_grep_last_S_last )
     {   my $i;
         for( $i = 0; $i != @_; $i++ )
-        {   if( $_[ $i ] ne $Q_files_I_grep_last_S_last[ $i ] )
-            {   last;
-            }
+        {   last if $_[ $i ] ne $Q_files_I_grep_last_S_last[ $i ];
         }
-        $ret = !$ret if $i == @_;
+        return 0 if $i == @_;
     }
-    #local $\ = $/;
-    #print "A: ${ret}: ${file_root}: ". join( '-', @_ ) . ' CMP '. join( '-', @Q_files_I_grep_last_S_last );
     @Q_files_I_grep_last_S_last = @_;
-    return $ret;
+    return 1;
 }
 #-------------------------------------------------------------------------------
 sub Q_files_I_grep_patch_I_root_ver
-{   my @root_ver;
-    my $before_numeric = 1;
-    foreach( @_ )
-    {   if( $before_numeric )
-        {   $before_numeric = !$before_numeric if /^[0-9]+$/;
-        }elsif( /^[a-z]/ )
-        {   last;
-        }
-        push @root_ver, $_;
+{   my $chromium = $_[0] =~ /^(?:${Q_files_S_re_version_1})/s;
+    my @root_ver = ();
+    my $i = 0;
+    $i++ if /^[0-9]+$/s;
+    while( $i != @_
+    and $_[ $i ] =~ /^[a-z]/is
+    )
+    {   push @root_ver, $_[ $i++ ];
     }
+    push @root_ver, $_[ $i++ ] if $i != @_ and $_[ $i ] =~ /^[0-9]/s;
+    push @root_ver, $_[ $i++ ] if !$chromium and $i != @_ and $_[ $i ] =~ /^[0-9]/s;
     return @root_ver;
 }
 sub Q_files_I_grep_patch
 {   my $file_root = Q_files_I_prepare_name(shift);
     local @_ = Q_files_I_split( $file_root );
-    my $ret = grep /^(?:${Q_files_S_re_patches})$/, @_;
-    return $ret unless $ret;
+    return 0 unless grep /^(?:${Q_files_S_re_patches})$/s, @_;
     my @root_ver = Q_files_I_grep_patch_I_root_ver( @_ );
     foreach( @Q_files_S_preserve )
     {   my @a = Q_files_I_split( Q_files_I_prepare_name( $_ ));
         my @root_ver_ = Q_files_I_grep_patch_I_root_ver( @a );
-        if( !grep( /^(?:${Q_files_S_re_patches})$/, @a ))
-        {   my $i;
-            for( $i = 0; $i != @root_ver and $i != @root_ver_; $i++ )
-            {   last if $root_ver[ $i ] ne $root_ver_[ $i ];
+        if( !grep( /^(?:${Q_files_S_re_patches})$/s, @a ))
+        {   my $i = 0;
+            my $j = 0;
+            while( $i != @root_ver and $j != @root_ver_ )
+            {   next if $root_ver[ $i ] =~ /^(?:${Q_files_S_re_patches})$/s;
+                last if $root_ver[ $i ] ne $root_ver_[ $j ];
+                $j++;
+            }continue
+            {   $i++;
             }
-            $ret = !$ret if $i == @root_ver or $i == @root_ver_;
+            return 1 if $i == @root_ver or $j == @root_ver_;
         }
     }
-    return $ret;
+    return 0;
 }
 #===============================================================================
 chdir $Q_files_S_dir_distfiles || die "Cannot change directory to distfiles: $!";
@@ -155,9 +155,9 @@ my @Q_files_S = grep { !/(?:^\.|\.__download__$)/s } readdir( $dh );
 closedir $dh;
 my @Q_files_S_cksum_fail = grep( /\._checksum_failure_\.[_0-9a-z]+$/s, @Q_files_S );
 @Q_files_S = sort Q_files_I_sort_I_cmp grep { !/\._checksum_failure_\.[_0-9a-z]+$/s } @Q_files_S;
-@Q_files_S_preserve = grep { !Q_files_I_grep_last( $_ ) } @Q_files_S;
+@Q_files_S_preserve = grep { Q_files_I_grep_last( $_ ) } @Q_files_S;
 @Q_files_I_grep_last_S_last = ();
-@Q_files_S = grep { Q_files_I_grep_last( $_ ) or Q_files_I_grep_patch( $_ ) } @Q_files_S;
+@Q_files_S = grep { !Q_files_I_grep_last( $_ ) and !Q_files_I_grep_patch( $_ ) } @Q_files_S;
 #-------------------------------------------------------------------------------
 unlink @Q_files_S_cksum_fail;
 mkdir $Q_files_S_dir_tmp_distfiles || die "Cannot create temporary distfiles directory: $!";
